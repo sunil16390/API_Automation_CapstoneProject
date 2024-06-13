53529f27d0a79946a6d48ee3f48e6db64565222a
sunil16390 <sunil16edu@gmail.com>
2024-06-14 03:32:34 +0530

    Update ReadFromXL.java

--- src/test/java/Reuseable/ReadFromXL.java
+++ src/test/java/Reuseable/ReadFromXL.java
@@ -1,9 +1,8 @@
 package Reuseable;
 
 import java.io.FileInputStream;
-import org.apache.poi.ss.usermodel.DataFormatter;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ReadFromXL {
@@ -12,6 +11,44 @@ public class ReadFromXL {
 //        int rowsinXl = xl.getExcelRowCount("CardDetails","CreditCards");
 //        System.out.println(rowsinXl);
 //    }
+public String read_And_Print_XL_AsPerTestData(String testcasename , String columnName){
+
+    String data = null;
+    try{
+        String XLFilePath = System.getProperty("user.dir")+"/Input/CardDetails.xlsx";
+        FileInputStream myxlfile = new FileInputStream(XLFilePath);
+        Workbook workbook = new XSSFWorkbook(myxlfile);
+        Sheet sheet = workbook.getSheet("CreditCards");
+        int lastRow = sheet.getLastRowNum();
+        // System.out.println("The last row which has data =="+lastRow);
+
+        // Loop for Row Iteration...
+        for(int i=0;i<=lastRow;i++){
+            Row row = sheet.getRow(i);
+            // Get the last Column which has data
+            int lastCell = row.getLastCellNum();
+            Cell cell = row.getCell(0);
+            String runtimeTestCaseName = cell.getStringCellValue();
+            //   System.out.println("The First Column Value is ==>"+runtimeTestCaseName);
+            if(runtimeTestCaseName.equals(testcasename)) {
+                // Loop for Column Iteration ...
+                Row RowNew = sheet.getRow(0);
+                for(int j=0;j<lastCell;j++){
+                    Cell cellnew = RowNew.getCell(j);
+                    String RunTimeCallValue = cellnew.getStringCellValue();
+                    if(RunTimeCallValue.equals(columnName)) {
+                        data = sheet.getRow(i).getCell(j).toString();
+                        // System.out.println("The XL value is ==>" + data);
+                    }
+                }
+            }
+        }
+    }
+    catch(Exception e){
+        e.printStackTrace();
+    }
+    return data;
+}
     public String getCellData(int rowNum, int colNum) {
         String value = null;
         try {
